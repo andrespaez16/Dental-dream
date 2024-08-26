d58a6800b8b1f6eb715e53fa88c7c9bbb2b6ad13
Andres Fernando Paez Cardenas <andres.paez+Siigo@siigo.com>
2024-08-25 23:44:07 -0500

    add new test

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,53 +1,29 @@
 import React from "react";
 import "./Hero.css";
-import arrow_icon from "../Assets/arrow.png";
+import { Animate } from "react-simple-animate";
 
 const Hero = () => {
   return (
     <div className="hero">
+      <div className="hero-left"></div>
       <div className="hero-left">
-      </div>
-      <div className="hero-left fade-in">
-        <div>
-          <div className="hero-hand-icon">
-            <p>Transformamos vidas creando sonrisas</p>
+        <Animate
+          play
+          duration={2}
+          end={{ opacity: 1, filter: "blur(0)" }}
+          start={{ opacity: 0, filter: "blur(10px)" }}
+        >
+          <div>
+            <div className="hero-hand-icon">
+              <p>Transformamos vidas creando sonrisas</p>
+            </div>
+            <p>Especialista en sonrisas naturales</p>
+            <p>en medellin.</p>
+            <button type="submit" className="hero-latest-btn">
+              Agenda tu cita !!!
+            </button>
           </div>
-          <p>Especialista en sonrisas naturales</p>
-          <p>en medellin.</p>
-        <button type="submit" className="hero-latest-btn">
-          Agenda tu cita !!!
-        </button>
-        </div>
-        {/* <section className="w-[400px] ml-[200px] mt-[50px]">
-          <form action="#" className="space-y-8">
-            <div>
-              <input
-                type="email"
-                id="email"
-                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light shadow-core"
-                placeholder="name@flowbite.com"
-                required
-              />
-            </div>
-            <div>
-              <input
-                type="text"
-                id="subject"
-                className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light shadow-core"
-                placeholder="Let us know how we can help you"
-                required
-              />
-            </div>
-            <div className="sm:col-span-2">
-              <textarea
-                id="message"
-                rows="6"
-                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 shadow-core"
-                placeholder="Leave a comment..."
-              ></textarea>
-            </div>
-          </form>
-        </section> */}
+        </Animate>
       </div>
     </div>
   );
--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -1,28 +1,44 @@
 import React from "react";
 import "./NewsLetter.css";
+import { Animate } from "react-simple-animate";
 const NewsLetter = () => {
   return (
     <div className="mision-and-vision">
-      <div className="mision">
-        <h2 className="mision-title">Misión:</h2>
-        <p className="mision-text">
-          Ser un consultorio odontológico en constante evolución, conformado por
-          los mejores especialistas en odontología, en constante formación,
-          logrando mejores resultados en cada tratamiento y así ofrecer una
-          atención personalizada a cada paciente. Nuestros valores son:
-          excelencia en el servicio, honestidad, eficacia y respeto.
-        </p>
-      </div>
-      <div className="vision">
-        <h2 className="vision-title">Visión:</h2>
-        <p className="vision-text">
-          Queremos ser reconocidos por nuestro profesionalismo y buen trato,
-          llegando así a ser líderes en la atención integral de nuestros
-          pacientes, la cual nos lleve a una proyección de crecimiento a nivel
-          nacional y así podamos ayudar a más personas a tener la sonrisa que
-          siempre han soñado.
-        </p>
-      </div>
+      <Animate
+        play
+        duration={2}
+        end={{ opacity: 1, filter: "blur(0)" }}
+        start={{ opacity: 0, filter: "blur(10px)" }}
+      >
+        <div className="mision">
+          <h2 className="mision-title">Misión:</h2>
+          <p className="mision-text">
+            Ser un consultorio odontológico en constante evolución, conformado
+            por los mejores especialistas en odontología, en constante
+            formación, logrando mejores resultados en cada tratamiento y así
+            ofrecer una atención personalizada a cada paciente. Nuestros valores
+            son: excelencia en el servicio, honestidad, eficacia y respeto.
+          </p>
+        </div>
+      </Animate>
+
+      <Animate
+        play
+        duration={2.5}
+        end={{ opacity: 1, filter: "blur(0)" }}
+        start={{ opacity: 0, filter: "blur(10px)" }}
+      >
+        <div className="vision">
+          <h2 className="vision-title">Visión:</h2>
+          <p className="vision-text">
+            Queremos ser reconocidos por nuestro profesionalismo y buen trato,
+            llegando así a ser líderes en la atención integral de nuestros
+            pacientes, la cual nos lleve a una proyección de crecimiento a nivel
+            nacional y así podamos ayudar a más personas a tener la sonrisa que
+            siempre han soñado.
+          </p>
+        </div>
+      </Animate>
     </div>
   );
 };
