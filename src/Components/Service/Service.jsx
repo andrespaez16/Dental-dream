be980836fef9283f2e9490ff30e2ba60e32520c0
Andres Fernando Paez Cardenas <andres.paez+Siigo@siigo.com>
2024-10-16 18:16:44 -0500

    changes

--- src/Components/Service/Service.jsx
+++ src/Components/Service/Service.jsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Service.css";
 import "../Assets/reabiltacion.jpg";
 import { useTranslation } from "react-i18next";
 const Service = (props) => {
   const [t, i18n] = useTranslation("global");
+  const [deviceMovil, setDeviceMovil] = useState();
 
-  console.log(props.info, "estaisoos");
+  const deviceSize = () => {
+    if (window.screen.height < 844) {
+      setDeviceMovil(true);
+    } else {
+      setDeviceMovil(false);
+    }
+  };
+
+  useEffect(() => {
+    deviceSize();
+  }, []);
+
   return (
     <div className="service">
-      {props.info.direction ? (
+      {deviceMovil ? (
         <div className="left-contain">
-          <div className="service-contain  fade-in-left">        
+          <div className="service-contain  fade-in-left">
+            <h2>{t(props.info.title)}</h2>
+            <p className="service-text">{t(props.info.text)}</p>
+          </div>
+          <div className={t(props.info.classPhoto)}></div>
+        </div>
+      ) : props.info.direction ? (
+
+        <div className="left-contain">
+          <div className="service-contain  fade-in-left">
             <h2>{t(props.info.title)}</h2>
             <p className="service-text">{t(props.info.text)}</p>
           </div>
